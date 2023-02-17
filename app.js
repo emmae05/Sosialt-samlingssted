15b7a8905a20500c45212c045375ce8c964b9aae
emmae05 <e-emmelk@iskule.no>
2023-02-17 13:05:23 +0100

    -_-

--- app.js
+++ app.js
@@ -4,12 +4,55 @@ const bcrypt = require("bcrypt");
 const path = require("path");
 const db = require("better-sqlite3")("database.db")
 
+
+
 const app = express();
+const password = 'passord';
+const saltRounds = 10;
+
+
+bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+        console.log(hash);
+    });
+});
+
+//Åpner databasen
+function connectToDb() {
+  db = sqlite3.Database('./sosialt-samlingssted.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the users datanase');
+});
+}
+
+
+//Oppretter og lagrer den nye brukeren
+function createUser (brukernavn, passord) {
+    //passord hash
+    bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+            return console.error(err);
+        }
+
+
+
+        //brukernavn og passord lagres i databasen
+        const sql = `INSERT INTO users (brukernavn, passord)
+        VALUES (?, ?)`;
+        db.run(sql, [brukernavn, hash], (err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(`Brukeren ${brukernavn} ble opprettet!`);
+        });
+    });
+}
 
 
 
 
 app.listen("3000", () => {
-    console.log("Server listening at http://localhost:3000!")
+    console.log("Server listening at http://localhost:3000")
 })
-
